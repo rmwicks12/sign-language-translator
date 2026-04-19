ff6914342a4546857750a32b259f0fe1586bc5a8
Rithwik Murthy <rithwikmurthy@gmail.com>
2026-04-19 17:20:42 +0530

    Added Data Collection Studio and backend saving pipeline

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from translator import views  # <--- Import your new view
+from translator import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # <--- Tell Django to load the homepage
+    path('', views.home, name='home'), 
+    path('collect/', views.collect_data, name='collect'),
+    path('save_dataset/', views.save_dataset, name='save_dataset'), # <--- Add this line
 ]
