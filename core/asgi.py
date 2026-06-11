16aeb65366aabb4b04ba70e20f677fb6519313a0
rmwicks12 <1dt24cs231@dsatm.edu.in>
2026-06-11 17:30:10 +0530

    configure ASGI server with Daphne and map real-time WebSocket routing

--- core/asgi.py
+++ core/asgi.py
@@ -2,10 +2,16 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import translator.routing
+
+# IMPORTANT: Change 'translator.routing' below to 'core.routing' if you put 
+# your consumers.py and routing.py inside the core folder instead!
+import translator.routing 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+# The ProtocolTypeRouter inspects incoming traffic.
+# If it's a standard web page load, it hands it to Django.
+# If it's a WebSocket connection, it hands it to Mudra's inference engine.
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
--- translator/routing.py
+++ translator/routing.py
@@ -1,6 +1,8 @@
 from django.urls import re_path
 from . import consumers
 
+# This is the WebSocket equivalent of standard Django urlpatterns
 websocket_urlpatterns = [
-    re_path(r'ws/translate/$', consumers.TranslationConsumer.as_asgi()),
+    # We use ws/ as a prefix to clearly separate WebSockets from standard http traffic
+    re_path(r'ws/translate/$', consumers.SignLanguageConsumer.as_asgi()),
 ]
